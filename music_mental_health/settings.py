27594b8b62448f46e30d2ec99a8dd829d4b7d83d
pronoyjoy <pronoy.das.developer@gmail.com>
2025-04-18 23:21:28 +0600

    database connection and migration

--- music_mental_health/settings.py
+++ music_mental_health/settings.py
@@ -90,11 +90,15 @@ WSGI_APPLICATION = 'music_mental_health.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': env.db(default='sqlite:///db.sqlite3')
 }
 
 
